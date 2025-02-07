958629230381e1ac6cce690fa40cbf5158996a23
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2025-02-07 11:40:25 +0100

    Fix Cpso Doctors Spider

--- gta_doctors/gta_doctors/spiders/cpso_doctors.py
+++ gta_doctors/gta_doctors/spiders/cpso_doctors.py
@@ -1,7 +1,7 @@
-# from scrapy import Spider
 import scrapy
 from selenium import webdriver
-# from scrapy.http import HtmlResponse
+from scrapy.http import HtmlResponse
+
 
 class CpsoDoctorsSpider(scrapy.Spider):
     name = "cpso_doctors"
@@ -14,33 +14,35 @@ class CpsoDoctorsSpider(scrapy.Spider):
     def parse(self, response):
         self.driver.get(response.url)
         # Fill the form using Selenium
-        self.driver.find_element_by_name('City').send_keys('Toronto')
-        self.driver.find_element_by_name('HospitalLocation').send_keys('Ajax')
-        self.driver.find_element_by_name('btnSearch').click()
+        self.driver.find_element_by_name("City").send_keys("Toronto")
+        self.driver.find_element_by_name("HospitalLocation").send_keys("Ajax")
+        self.driver.find_element_by_name("btnSearch").click()
 
         # Waiting for results
         from selenium.webdriver.common.by import By
         from selenium.webdriver.support.ui import WebDriverWait
         from selenium.webdriver.support import expected_conditions as EC
+
         WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'div.doctor-info'))
+            EC.presence_of_element_located((By.CSS_SELECTOR, "div.doctor-info"))
         )
 
         # Send HTML to Scrapy
         html = self.driver.page_source
-        yield HtmlResponse(url=self.driver.current_url, body=html, encoding='utf-8')
-        
+        yield HtmlResponse(url=self.driver.current_url, body=html, encoding="utf-8")
 
     def parse_results(self, response):
         # Aqui você processa os resultados da busca
-        for doctor in response.css('div.doctor-info'):  # Ajuste o seletor conforme o HTML da página
+        for doctor in response.css(
+            "div.doctor-info"
+        ):  # Ajuste o seletor conforme o HTML da página
             yield {
-                'name': doctor.css('h2.name::text').get(),
-                'specialization': doctor.css('p.specialization::text').get(),
-                'address': doctor.css('p.address::text').get(),
+                "name": doctor.css("h2.name::text").get(),
+                "specialization": doctor.css("p.specialization::text").get(),
+                "address": doctor.css("p.address::text").get(),
             }
 
         # Paginação (se houver)
-        next_page = response.css('a.next-page::attr(href)').get()
+        next_page = response.css("a.next-page::attr(href)").get()
         if next_page:
             yield response.follow(next_page, self.parse_results)
